ee0dac2320b586aed9c22eb082786680bb583514
Akshay Anil Kumar <akshay2k2@gmail.com>
2026-04-16 10:19:52 +0000

    Phase 4: Added parent commit detection using HEAD

--- commit.c
+++ commit.c
@@ -197,4 +197,6 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     ObjectID tree_id;
 
     if (tree_from_index(&tree_id) != 0) return -1;
+    ObjectID parent_id;
+    int has_parent = (head_read(&parent_id) == 0);
 }
